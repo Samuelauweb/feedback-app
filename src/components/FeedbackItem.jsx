01b2e134d944e1e49f0d8257017d3a639eb1eb36
Samuel <samuel.cflau@gmail.com>
2022-02-25 18:01:07 +0800

    15. Managing Global State

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,23 +1,13 @@
 import React from 'react'
-import { useState } from 'react'
 
-function FeedbackItem() {
-  const [rating, setRating] = useState(7)
-  const [text, setText] = useState('This is an example of a feeback item')
-
-  const handleClick = () => {
-    setRating((prevValue) => {
-      return prevValue + 1
-    })
-  }
+function FeedbackItem({item}) {
 
   return (
     <div className='card'>
-      <div className="num-display">{rating}</div>
+      <div className="num-display">{item.rating}</div>
       <div className="text-display">
-        {text}
+        {item.text}
       </div>
-      <button onClick={handleClick}>Click</button>
     </div>
   )
 }
