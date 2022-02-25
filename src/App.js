955cca5f89fd9ee885080522aaf4469c3947602c
Samuel <samuel.cflau@gmail.com>
2022-02-25 19:04:32 +0800

    16. <Card> Component & Conditional Styles 10 分鐘 (Typechecking With PropTypes)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { useState } from "react"
-import Header from "./components/Header"
-import FeedbackList from "./components/FeedbackList"
-import FeedbackData from "./data/FeedbackData"
+import { useState } from 'react'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackData from './data/FeedbackData'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
 
   return (
     <>
-    <Header/>
-    <div className='container'>
-      <FeedbackList feedback={feedback}/>
-    </div>
+      <Header />
+      <div className='container'>
+        <FeedbackList feedback={feedback} />
+      </div>
     </>
   )
 }
