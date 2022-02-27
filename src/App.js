4e16475b0b2c7c28b522a9d676411a8ecec8be02
Samuel <samuel.cflau@gmail.com>
2022-02-27 15:00:13 +0800

    27. Creating Links (v5 & v6) 5 分鐘 (AboutIconLink.jsx; <Link>)

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
 import FeedbackData from './data/FeedbackData'
 import AboutPage from './pages/AboutPage'
+import AboutIconLink from './components/AboutIconLink'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -39,6 +40,8 @@ function App() {
                   feedback={feedback}
                   handleDelete={deleteFeedback}
                 />
+                <AboutIconLink />
+                {/* React Router v6 */}
               </>
             }
           ></Route>
