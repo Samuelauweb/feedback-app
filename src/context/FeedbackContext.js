d6d328698d6a5bb9433ebe0fc1b8088a27dc0fb2
Samuel <samuel.cflau@gmail.com>
2022-02-28 23:09:14 +0800

    40. Fetch Data From JSON-Server Backend 5 分鐘 (fetchFeedback; isLoading)

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,30 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: 'This item is from context',
-      rating: 10,
-    },
-    {
-      id: 2,
-      text: 'This item 2 is from context',
-      rating: 9,
-    },
-    {
-      id: 3,
-      text: 'This item 3 is from context',
-      rating: 7,
-    },
-  ])
-
+  const [isLoading, setIsLoading] = useState(true)
+  const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {}, // when the icon is clicked, the content about that item will be put in here
     edit: false, // when the icon is clicked, it will change to 'true', which means we are in edit mode
   })
+
+  useEffect(() => {
+    fetchFeedback()
+  }, []) // when the page is loaded, it will run
+
+  // Fetch feedback
+  const fetchFeedback = async () => {
+    const response = await fetch(
+      `http://localhost:5000/feedback?_sort=id&_order=desc`
+    )
+    const data = await response.json()
+
+    setFeedback(data)
+    setIsLoading(false)
+  }
 
   // Delete feedback
   const deleteFeedback = (id) => {
@@ -62,6 +61,8 @@ export const FeedbackProvider = ({ children }) => {
       value={{
         feedback,
         feedbackEdit,
+        isLoading,
+        fetchFeedback,
         deleteFeedback,
         addFeedback,
         editFeedback,
