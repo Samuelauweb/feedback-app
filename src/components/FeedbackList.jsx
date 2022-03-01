53a5c946576d5cf62fe422a0e3ea0ae639ffa116
Samuel <samuel.cflau@gmail.com>
2022-03-01 11:07:00 +0800

    41. Spinner Component 6 分鐘 (isLoading; Spinner.jsx)

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { useContext } from 'react'
-import FeedbackContext from '../context/FeedbackContext'
 import { motion, AnimatePresence } from 'framer-motion'
+import FeedbackContext from '../context/FeedbackContext'
 import FeedbackItem from './FeedbackItem'
+import Spinner from './shared/Spinner'
 // import PropTypes from 'prop-types'
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext)
+  const { feedback, isLoading } = useContext(FeedbackContext)
 
-  if (!feedback || feedback.length === 0) {
+  if (!isLoading && (!feedback || feedback.length === 0)) {
     return <p>No feedback yet</p>
   }
 
   // With Framer-motion
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className='feedback-list'>
       <AnimatePresence>
         {feedback.map((item) => {
@@ -35,6 +38,7 @@ function FeedbackList() {
       </AnimatePresence>
     </div>
   )
+
   // Without Framer-motion
   // return (
   //   <div className='feedback-list'>
